64816abe130f59c5736094fce307da64bd87e276
Kyle McAlpine <k@kylejm.io>
2017-06-07 20:08:28 +0100

    Adding CoreNFC

--- Tap/ViewController.swift
+++ Tap/ViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
-
+    var session: NFCNDEFReaderSession?
+    
+    override init(nibName: String?, bundle: Bundle?) {
+        super.init(nibName: nibName, bundle: bundle)
+        initialise()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialise()
+    }
+    
+    func initialise() {
+        session = NFCNDEFReaderSession(delegate: self, queue: DispatchQueue.global(), invalidateAfterFirstRead: false)
+        session?.begin()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 
 
 }
 
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error)
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        print(messages)
+    }
+}
